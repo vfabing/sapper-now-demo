9b983d13b8d59dc7d9a2a56a7847109af70aa4b4
Vivien FABING <vfabing@infinitesquare.com>
2019-06-07 11:05:51 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import sirv from 'sirv';
+// import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
@@ -6,12 +6,17 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+const app = polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-		sirv('static', { dev }),
+		// sirv('static', { dev }),
 		sapper.middleware()
 	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+
+export default app.handler // Remove .handler when using Express
+
+if (!process.env.NOW_REGION) {
+	app.listen(PORT, err => {
+		if (err) console.log('error', err)
+	})
+}
